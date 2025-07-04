442373d01d462d897bbcbfd604fbc218daf1e8af
lily <hueduong288@gmail.com>
2025-07-04 21:45:11 +1000

    set up connect with DB, mapping User and test create and get a user (task 7)

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask_restx import Api
-from flask_sqlalchemy import SQLAlchemy
 from config import config
+from app.extensions import db
 from app.api.v1.users import api as users_api
 from app.api.v1.amenities import api as amenities_api
 from app.api.v1.places import api as places_api
 from app.api.v1.reviews import api as reviews_api
 
-db = SQLAlchemy()
-
-def create_app(config_class=config.development):
+def create_app(config_class=config['development']):
     app = Flask(__name__)
+
+    # Database configuration
+    USER = "hbnb_p3"
+    PWD = "hbnb_p3_pw"
+    HOST = "localhost"
+    DB = "hbnb_p3_db"
+
+    # Configure SQLAlchemy to connect SQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{USER}:{PWD}@{HOST}/{DB}'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize db with app
+    db.init_app(app)
+
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -6,7 +6,9 @@ class BaseModel(db.Model):
     __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+
     created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=lambda: datetime.now(timezone.utc),
                            onupdate=lambda: datetime.now(timezone.utc))
